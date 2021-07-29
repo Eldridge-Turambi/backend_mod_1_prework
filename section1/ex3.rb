5cfc4472b1a6f6183dde75d023f446fc451137b5
Eldridge Turambi <eturambi95@gmail.com>
2021-07-29 16:58:57 -0600

    Addition of comments and 'study drills' section

--- section1/ex3.rb
+++ section1/ex3.rb
@@ -1,23 +1,40 @@
+#This will print out whatever is inside the quotes
 puts "I will now count my chickens:"
 
+#This will prunt out what ever is wriiten in green and the solution of the equation inside the #{}
 puts "Hens #{25 + 30 / 6}"
 puts "Roosters #{100 - 25 * 3 % 4}"
 
+#This will print whatever is in the quotes
 puts "Now I will count the eggs:"
 
+#This will simply run the equation given and print out the answer
 puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
 
+#See line 1
 puts "Is it true that 3 + 2 < 5 - 7?"
 
+#Since this is an equation using a '<' or '>' sign, this will print out if the differential is true or false
 puts 3 + 2 < 5 - 7
 
+#See line 4. It will print the answer inside the [] since there are block parentheses inside.
 puts "What is 3 + 2? #{[3 + 2]}"
 puts "What is 5 - 7 #{[5 - 7]}"
 
+#See line 1
 puts "Oh, that's why it's false."
 
+#See line 1
 puts "How about some more."
 
+#It will print whatever is in quotes and will print the outputs of either 'true' or 'false' depending on what is inside the #{}
 puts "Is it greater? #{5 > -2}"
 puts "Is it greater or equal? #{5 >= -2}"
 puts "Is it less or equal? #{5 <= -2}"
+
+# Study Drills part starts here
+
+# "Something new to calculate"
+puts "How many weeks will it take to complete 4 modules at Turing? #{7 * 4}"
+puts "I want 1 slice/s of pizza"
+puts "How many slices of pizza should I grab from the table? #{100 % 33}"
