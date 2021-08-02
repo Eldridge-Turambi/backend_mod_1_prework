5e80d0a76aa9c09b3a9f33453e0f10a0149c676c
Eldridge Turambi <eturambi95@gmail.com>
2021-08-01 21:37:16 -0600

    Add all complete excercises from section 1

--- section1/exercises/interpolation.rb
+++ section1/exercises/interpolation.rb
@@ -8,23 +8,29 @@
 name = "Ron"
 team = "Chudley Cannons"
 
-p "The #{team} are #{name}'s favorite Quidditch team"
+puts "The #{team} are #{name}'s favorite Quidditch team"
+
 
 # Write code that uses the variables below to form a string that reads
 # "The quick red fox jumped over the lazy brown dog":
 speedy = "quick red fox"
 slow_poke = "lazy brown dog"
 
-p # YOUR CODE HERE
+puts "The #{speedy} jumped over the #{slow_poke}." # YOUR CODE HERE
 
 # Write code that uses the variables below to form a string that reads
 # "In a predictable result, the tortoise beat the hare!":
-slow_poke = "tortoise"
-speedy = "hare"
+slow_poke_1 = "tortoise"
+speedy_1 = "hare"
 
-# YOUR CODE HERE
+puts "In a predicable result, the #{slow_poke_1} beat the #{speedy_1}!"# YOUR CODE HERE
 
 
 # YOU DO:
 # Declare three variables, name/content/data type of your choice. Think carefully about what you name the variables. Remember, the goal is to be concise but descriptive (it's a hard balance!) Then, log out ONE sentence that incorporates all THREE variables.
 
+my_name = "Eldridge"
+sandwich_type = "Turkey"
+number_of_sandwiches = 3
+
+puts "Yesterday, #{my_name} was so hungry that he made and ate #{number_of_sandwiches} sandwiches with #{sandwich_type} in it. Yum!"
