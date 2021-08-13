857f83c363eab300f3e08dc510e9e6ccf4db4f6f
Eldridge Turambi <87398716+Eldridge-Turambi@users.noreply.github.com>
2021-08-12 22:00:20 -0400

    Update annotations.rb

--- final_prep/annotations.rb
+++ final_prep/annotations.rb
@@ -3,12 +3,14 @@
 # Use the # to create a new comment
 
 # Build a Bear
-
+# making a new method called build_a_bear
 def build_a_bear(name, age, fur, clothes, special_power)
+  # establishing variables below: different from arguments in line 7
   greeting = "Hey partner! My name is #{name} - will you be my friend?!"
   demographics = [name, age]
   power_saying = "Did you know that I can #{special_power}?"
   built_bear = {
+  # attributes of the bear 
     'basic_info' => demographics,
     'clothes' => clothes,
     'exterior' => fur,
@@ -18,21 +20,24 @@ def build_a_bear(name, age, fur, clothes, special_power)
   }
   return built_bear
 end
-
+# making 2 different instances of the bear
 build_a_bear('Fluffy', 4, 'brown', ['pants', 'jorts', 'tanktop'], 'give you nightmares')
 build_a_bear('Sleepy', 2, 'purple', ['pajamas', 'sleeping cap'], 'sleeping in')
 
 
 # FizzBuzz
-
+# method fizzbuzz
 def fizzbuzz(num_1, num_2, range)
+#in line below: It goes through each element of the 3 arguments and from there it decides whether or not to pit fizzbuzz, fizz, or buzz depending on the constraints given
   (1..range).each do |i|
     if i % num_1 === 0 && i % num_2 === 0
       puts 'fizzbuzz'
+      # use the elsif after the 'if'
     elsif i % num_1 === 0
       puts 'fizz'
     elsif i % num_2 === 0
       puts 'buzz'
+    # in the last one, use 'else' if does not satisfy any of the conditionals sent prior
     else
       puts i
     end
